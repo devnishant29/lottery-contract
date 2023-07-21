96fdad47e1a235ce8673b9abc0b1fbf74e9ecb9f
devnishant29 <devnishant29@gmail.com>
2023-07-21 16:45:34 +0530

    lottery contract with basic frontend

---
+++ backend/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "ghost panther nasty fantasy upgrade runway develop file path dish word royal",
+  "https://sepolia.infura.io/v3/4d859ef53a994aa6a82566a7ef1fa44a"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
